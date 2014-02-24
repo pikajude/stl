20b422e6b4073e75b98e5c56542bd707248426ce
Joel Taylor <me@joelt.io>
2014-02-24 14:07:40 -0800

    quickchecks + token metadata

--- lexer.hs
+++ lexer.hs
@@ -1,87 +1,154 @@
 import Control.Applicative
 import Data.Char
-import Data.HashSet (fromList)
+import Data.Function
+import Data.List
+import Data.Monoid
+import Data.Ord
 import Data.Text (Text, pack)
 import Prelude hiding (lex, until)
 import System.IO
-import Text.Parser.Token.Highlight hiding (Comment)
+import Test.QuickCheck
+import Test.QuickCheck.Instances ()
 import Text.Trifecta hiding (symbol, symbolic, token)
+import Text.Trifecta.Delta
 
-data Keyword = K_Choose | K_If | K_Else deriving Show
+data Keyword = K_Choose | K_If | K_Else deriving (Show, Eq, Enum, Bounded)
 
-data Punctuation = P_Colon | P_Force | P_Equals
-                 | P_Lbracket | P_Rbracket
-                 | P_Lbrace | P_Rbrace
-                 | P_Lparen | P_Rparen
-                 | P_Comma
+data Punctuation = P_Cons | P_Colon | P_Force | P_Equals
+                 | P_LBracket | P_RBracket
+                 | P_LBrace | P_RBrace
+                 | P_LParen | P_RParen
+                 | P_Comma | P_Dot
                  | P_Arrow
-                 deriving Show
+                 deriving (Show, Eq, Enum, Bounded)
 
 data Comm = Single Text
           | Multiline Text
-          deriving Show
+          deriving (Show, Eq)
 
 data Literal = L_Str Text
-             deriving Show
+             deriving (Show, Eq)
 
-data Tok = Ident Text
-         | Comment Comm
-         | Separator
-         | Punct Punctuation
-         | Lit Literal
-         | KW Keyword deriving Show
+data PlainTok = Ident Text
+              | Comment Comm
+              | Separator
+              | Punct Punctuation
+              | Lit Literal
+              | KW Keyword deriving (Show, Eq)
 
+
+data Token = Token
+           { tTok :: PlainTok
+           , tPos :: (Delta,Delta)
+           } deriving (Show, Eq)
+
+type TokenStream = [Token]
+
+symbolic :: Char -> Parser Char
 symbolic c = token (char c)
 
+symbol :: String -> Parser String
 symbol c = token (string c)
 
+realSpace :: Parser String
 realSpace = many $ satisfy (\x -> x /= '\n' && isSpace x)
 
-token m = m <* realSpace
+token :: a -> a
+token m = m
 
+until :: Alternative f => f b -> f a -> f [a]
 until p m = [] <$ p <|> liftA2 (:) m (until p m)
 
-lex = Separator <$ (char '\n' <|> char ';' <?> "separator") <* realSpace
+ptok :: Parser PlainTok
+ptok = Separator <$ (char '\n' <|> char ';' <?> "separator") <* realSpace
 
-  <|> token (Ident . pack <$>
-          liftA2 (:)
-              (lower <|> char '_')
-              (many $ alphaNum <|> char '_' <|> char '.')
-          <?> "ident"
+   <|> token (Ident . pack <$>
+           liftA2 (:)
+               (lower <|> char '_')
+               (many $ alphaNum <|> char '_')
+           <?> "ident"
+             )
+
+   <|> fmap Comment (Multiline . pack
+                         <$> (string "###" *> (until (string "###") anyChar))
+                 <|> Single . pack <$> (char '#' *> many (notChar '\n'))
+                 <?> "comment"
+                    )
+
+   <|> fmap Punct (P_Cons <$ (symbol "::" <|> symbol "∷")
+               <|> P_Arrow <$ (symbol "->" <|> symbol "→")
+               <|> P_Force <$ symbolic '!'
+               <|> P_Colon <$ symbolic ':'
+               <|> P_Dot <$ symbolic '.'
+               <|> P_Equals <$ symbolic '='
+               <|> P_LBracket <$ symbolic '['
+               <|> P_RBracket <$ symbolic ']'
+               <|> P_LParen <$ symbolic '('
+               <|> P_RParen <$ symbolic ')'
+               <|> P_LBrace <$ symbolic '{'
+               <|> P_RBrace <$ symbolic '}'
+               <|> P_Comma <$ symbolic ','
+               <?> "punctuation")
+
+   <|> fmap KW (K_Choose <$ symbol "choose"
+            <|> K_If <$ symbol "if"
+            <|> K_Else <$ symbol "else"
+            <?> "keyword"
             )
 
-  <|> fmap Comment (Multiline . pack
-                        <$> (string "###" *> (until (string "###") anyChar))
-                <|> Single . pack <$> (char '#' *> many (notChar '\n'))
-                   )
-
-  <|> fmap Punct (P_Colon <$ symbolic ':'
-              <|> P_Force <$ symbolic '!'
-              <|> P_Equals <$ symbolic '='
-              <|> P_Lbracket <$ symbolic '['
-              <|> P_Rbracket <$ symbolic ']'
-              <|> P_Lparen <$ symbolic '('
-              <|> P_Rparen <$ symbolic ')'
-              <|> P_Lbrace <$ symbolic '{'
-              <|> P_Rbrace <$ symbolic '}'
-              <|> P_Comma <$ symbolic ','
-              <|> P_Arrow <$ symbol "->"
-              <?> "punctuation")
-
-  <|> fmap KW (K_Choose <$ symbol "choose"
-           <|> K_If <$ symbol "if"
-           <|> K_Else <$ symbol "else"
-           <?> "keyword"
-           )
-
-  <|> fmap Lit (L_Str . pack . concat <$>
-                    token (between (char '"') (char '"')
-                        (many (esc <|> fmap return (notChar '"'))))
-            <?> "literal"
-               )
+   <|> fmap Lit (L_Str . pack . concat <$>
+                     token (between (char '"') (char '"' <?> "end of string")
+                         (many (esc <|> fmap return (noneOf "\"\n"))))
+             <?> "literal"
+                )
     where esc = do
-            c <- char '\\'
+            c <- char '\\' <?> "backslash"
             m <- anyChar
             return [c,m]
 
-main = withFile "main.stl" ReadMode $ \h -> parseTest (some lex <* eof) =<< hGetContents h
+tok :: Parser Token
+tok = do
+    before <- position
+    m <- ptok
+    after <- position
+    _ <- realSpace
+    return $ Token m (before,after)
+
+normalize :: TokenStream -> TokenStream
+normalize = dropSeps . collapseSeps
+
+collapseSeps :: TokenStream -> TokenStream
+collapseSeps = concatMap (\xs -> if tTok (head xs) == Separator then take 1 xs else xs)
+             . groupBy ((==) `on` tTok)
+
+dropSeps :: TokenStream -> TokenStream
+dropSeps (t:ts) | all ((== Separator) . tTok) ts = [t]
+                | otherwise = t:dropSeps ts
+dropSeps [] = []
+
+main :: IO ()
+main = withFile "main.stl" ReadMode $ \h ->
+    parseTest (fmap normalize $ some tok <* eof) =<< hGetContents h
+
+test :: IO ()
+test = do
+    quickCheck $ \x -> length (collapseSeps x) <= length x
+    quickCheck $ \x -> null x || tTok (last (dropSeps x)) /= Separator
+
+instance Arbitrary Token where
+    arbitrary = Token <$> arbitrary <*> pure mempty
+
+instance Arbitrary PlainTok where
+    arbitrary = oneof [genIdent, pure Separator, genPunc, genLit, genKW, genComm] where
+        genIdent = do
+            start <- elements $ '_' : ['a'..'z']
+            chars <- vectorOf 10 . elements $ ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "_"
+            return . Ident . pack $ start : chars
+
+        genComm = Comment <$> oneof [Single <$> arbitrary, Multiline <$> arbitrary]
+
+        genPunc = Punct <$> elements [minBound..maxBound]
+
+        genLit = Lit <$> oneof [L_Str <$> arbitrary]
+
+        genKW = KW <$> elements [minBound..maxBound]
